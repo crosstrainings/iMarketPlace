33ec11c2d6ae17d34c056b22357f5e65603c67f3
Irshad Ahmed <irshad.ahmed@contour-software.com>
2019-08-18 18:32:14 +0500

    IA: iMarketPlace Init

--- MarketPlace.Entities/Common/Address.cs
+++ MarketPlace.Entities/Common/Address.cs
@@ -1,6 +1,6 @@
 ﻿namespace MarketPlace.Entities.Common
 {
-    public class Address
+    public  class Address
     {
         public string StreetAddress { get; set; }
         public string HouseNumber { get; set; }
