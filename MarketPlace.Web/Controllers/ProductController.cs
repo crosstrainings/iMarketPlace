8bad3538fa64b9f9ecfebd86676084d652d07e90
Irshad Ahmed <irahmed@corp.contoursoftware.net>
2019-10-26 14:52:18 +0500

    IA: Topic: 1: Database (What | Why | How 2: SQL Queries 3: ADO.NET (Connection | Command | Data Reader)

--- MarketPlace.Web/Controllers/ProductController.cs
+++ MarketPlace.Web/Controllers/ProductController.cs
@@ -3,6 +3,29 @@ using System.Web.Mvc;
 
 namespace iMarketPlace.Web.Controllers
 {
+    /*Permanent Storage:
+     *- File System
+     *- Database System
+     *      - Technique / Concepts
+     *      -   MYSQL (SQL) - MSSQL (SQL) - Oracle (SQL) - MongoDB (No SQL) - Hadoop
+     *        - SQL (Structured Query Language)
+     *          - Language To Communicate with Database
+     *      - Providers / Vendors
+     *      - Oracle - Microsoft - Oracle - //
+     *      
+     *- MSSQL 
+     * - Tools
+     *  - SQL Server 2016 - 2014
+     *  - MSSQL (SQL Management Server ) (Browser)
+     *
+     * - How to Connect with SQL Server from Code 
+     * - ADO.NET (Connection - Command - Read|Write) 
+     * - ORM 
+     *
+     */
+
+
+
     public class ProductController : Controller
     {
 
--- MarketPlace.Web/Models/AdvertisementManager.cs
+++ MarketPlace.Web/Models/AdvertisementManager.cs
@@ -1,32 +1,48 @@
 ﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace iMarketPlace.Web.Models
 {
     public static class AdvertisementManager
     {
-        private static List<Advertisement> advertisements { get; set; }
         static AdvertisementManager()
         {
-            advertisements = new List<Advertisement>();
         }
 
 
         public static bool Add(Advertisement advertisement)
         {
-            if (advertisement.Category > 0 && advertisement.Image.Length > 0 && advertisement.Title.Length > 0 && advertisement.Price.Length > 0)
-            {
-                advertisements.Add(advertisement);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            SqlConnection connection = new SqlConnection("Data Source=CRLHL-AHMEDIRS2;Initial Catalog=iMarketPlaceDataBase;Integrated Security=True");
+            SqlCommand command = new SqlCommand($@"INSERT INTO Advertisements(Title,Price,Category,[Image])
+                                                        VALUES('{advertisement.Title}', '{advertisement.Price}', 
+                                                        {advertisement.Category}, '{advertisement.Image}')",connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            return true;
         }
-    
+
         public static List<Advertisement> Get()
         {
+            List<Advertisement> advertisements= new List<Advertisement>();
+            SqlConnection connection = new SqlConnection("Data Source=CRLHL-AHMEDIRS2;Initial Catalog=iMarketPlaceDataBase;Integrated Security=True");
+            SqlCommand command = new SqlCommand($@"SELECT * FROM Advertisements", connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Advertisement ad = new Advertisement();
+
+                ad.Title = reader.GetString(0);
+                ad.Price = reader.GetString(1);
+                ad.Category = reader.GetInt32(2);
+                ad.Image = reader.GetString(3);
+                advertisements.Add(ad);
+            }
+
             return advertisements;
+
+
         }
     }
 }
