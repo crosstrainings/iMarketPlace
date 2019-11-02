ef854495c5416eca140fc9b72372737a78b0db15
Irshad Ahmed <irahmed@corp.contoursoftware.net>
2019-11-02 15:07:31 +0500

    IA: Topics:
    - ORM - EF
    
    - How to use EF
      - References
      - Connection
      - Convention | Data Annotation | Fluent API

--- MarketPlace.Web/Controllers/UserController.cs
+++ MarketPlace.Web/Controllers/UserController.cs
@@ -3,6 +3,50 @@ using System.Web.Mvc;
 
 namespace iMarketPlace.Web.Controllers
 {
+    /*
+     *  ADO.NET
+     *  ORM (Object Relation Model)
+     *      Translate Objects To Queries and Queries To Objects
+     *
+     *      EF  (Entity Framework)
+     *      nHibernate
+     *      Micro ORM 
+     *
+     *
+     *  How to Add EF:
+     *  Nuget Package Server | NPM 
+     *  DLL
+     *
+     *
+     *  EF (Entity Framework)
+     *  Steps:
+         *  Connection
+         *  Scheme Structure
+         *  Operations
+         *
+     *
+     * Pros and Cons of ORM - Entity Framework
+     *
+     * Advantages:
+     *  Easy To Install / Use
+     *  Cleaner / Lesser Code
+     *  No SQL Knowledge require
+     *  In Programmer's Favor
+     *  Productivity 
+     *
+     *
+     *
+     * Disadvantages
+     *  Performance Low
+     */
+
+
+
+
+
+
+
+
     public class UserController : Controller
     {
         public ActionResult Login(string userName, string password)
