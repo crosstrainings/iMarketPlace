a496a37ce5451389fbba73493091373f013a5f15
Irshad Ahmed <irahmed@corp.contoursoftware.net>
2019-11-03 19:25:08 +0500

    IA: Topic: 1: Revise 2: EF Core Concepts 3: Navigation Entities 4: Migration Concepts - Issue Needs to revise 5: Annotations | Fluent | Convention 6: Data Loading | Lazy - Eager - Explicit

--- MarketPlace.Web/Models/AdvertisementContext.cs
+++ MarketPlace.Web/Models/AdvertisementContext.cs
@@ -9,7 +9,11 @@ namespace iMarketPlace.Web.Models
     {
         public AdvertisementContext() : base("name=iMarketPlaceConn")
         {
-
+            //Data Loading 
+            //1: Lazy Loading - By Default TRUE
+            //2: Eager Loading
+            //3: Explicit Loading
+            base.Configuration.LazyLoadingEnabled = false;
         }
 
 
@@ -17,13 +21,15 @@ namespace iMarketPlace.Web.Models
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
 
-            modelBuilder.Entity<Advertisement>().HasKey(x => x.RegistrationNumber);
-
+            //modelBuilder.Entity<Advertisement>().HasKey(x => x.RegistrationNumber)
+            //                                    .HasRequired(x => x.Title);
+            //modelBuilder.Entity<Advertisement>().Property(x=>x.Title).HasMaxLength(100);
             base.OnModelCreating(modelBuilder);
         }
 
 
         public DbSet<Advertisement> Advertisements { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
 
     }
