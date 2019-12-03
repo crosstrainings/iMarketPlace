d8fcb80b816ffd7f01855c4fc671d7745142d00e
Irshad Ahmed <irahmed@corp.contoursoftware.net>
2019-12-03 16:19:50 +0500

    IA: Upgrades - Admin Area Added - Login | Registration Form Updates - Home Screen Updates - Removed Bootstrap Theme File - Advert Card Updates - Code Refractorings

---
+++ MarketPlace.ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MarketPlace.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "required")]
+        public string Password { get; set; }
+    }
+}
--- MarketPlace.Web/Areas/Administration/Controllers/AdminController.cs
+++ MarketPlace.Web/Areas/Administration/Controllers/AdminController.cs
@@ -1,12 +1,37 @@
-﻿using System.Web.Mvc;
+﻿using iMarketPlace.Web.Controllers;
+using MarketPlace.ViewModels.Admin;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace iMarketPlace.Web.Areas.Administration.Controllers
 {
-    public class AdminController : Controller
+    public class AdminController : BaseController
     {
         // GET: Administration/Admin
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult AllAdverts()
+        {
+            List<AdvertisementViewModel> allRecipes = new List<AdvertisementViewModel>();
+            var advertisements = _advertisementService.Get();
+            foreach (var ad in advertisements)
+            {
+                allRecipes.Add(new AdvertisementViewModel()
+                {
+                    Category = ad.Category.Name,
+                    SubCategory = ad.Category.SubCategory.Name,
+                    ImgUrl = ad.Images.First().Url,
+                    Id = ad.Id,
+                    Price = ad.Price.ToString(".00"),
+                    Title = ad.Title,
+                });
+            }
+
+            ViewBag.AllRecipes = allRecipes;
+
             return View();
         }
     }
