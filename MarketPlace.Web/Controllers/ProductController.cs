56aa7d634770c94af915ecd633cdb94a59b78c70
Irshad Ahmed <irahmed@corp.contoursoftware.net>
2019-12-07 15:49:02 +0500

    IA: Topics - Window Services overview - Web Scrapping - SignalR - Code Documentation - Code Refractorings

--- MarketPlace.Web/Controllers/ProductController.cs
+++ MarketPlace.Web/Controllers/ProductController.cs
@@ -5,11 +5,24 @@ using System.Web.Mvc;
 
 namespace iMarketPlace.Web.Controllers
 {
+    /// <summary>
+    /// SOME SUMMARY HERE
+    /// </summary>
+    /// <seealso cref="iMarketPlace.Web.Controllers.BaseController" />
     public class ProductController : BaseController
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductController"/> class.
+        /// </summary>
         public ProductController()
         {
         }
+
+        /// <summary>
+        /// Creates the specified advertisement.
+        /// </summary>
+        /// <param name="advertisement">The advertisement To Add An Object.</param>
+        /// <returns></returns>
         public ActionResult Create(Advertisement advertisement)
         {
             var user = GetSession<UserSessionInfo>(USER_SESSION);
@@ -18,6 +31,12 @@ namespace iMarketPlace.Web.Controllers
             return RedirectToAction("Profile", "User");
         }
 
+        /// <summary>
+        /// Loads the specified skip.
+        /// </summary>
+        /// <param name="skip">The skip.</param>
+        /// <param name="take">The take.</param>
+        /// <returns></returns>
         public ActionResult Load(int skip = 0, int take = 10)
         {
             var advertisements = _advertisementService.Get().Skip(skip).Take(take).ToList();
